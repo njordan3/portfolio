a9d794e72402dd7175b61675a1169bb96fb0f98c
Nicholas Jordan <nijordan99@gmail.com>
2024-10-07 22:51:23 -0700

    please work

--- server.js
+++ server.js
@@ -1,42 +1,17 @@
-import { createRequestHandler } from "@remix-run/express";
+import remix from "@remix-run/express";
 import express from "express";
+import * as build from "./index.js";
 
-console.log('STARTING SERVER');
+const app = express();
+const port = 3000;
 
-try {
-  console.log('NODE_ENV', process.env.NODE_ENV);
-  const viteDevServer =
-  process.env.NODE_ENV === "production"
-    ? null
-    : await import("vite").then((vite) =>
-        vite.createServer({
-          server: { middlewareMode: true },
-        })
-      );
+app.all(
+  "*",
+  remix.createRequestHandler({
+    build,
+  })
+);
 
-  console.log('here1');
-  const app = express();
-  app.use(
-    viteDevServer
-      ? viteDevServer.middlewares
-      : express.static("static")
-  );
-
-  console.log('here2');
-  const build = viteDevServer
-    ? () =>
-        viteDevServer.ssrLoadModule(
-          "virtual:remix/server-build"
-        )
-    : await import("./index.js");
-
-  console.log('here3');
-  app.all("*", createRequestHandler({ build }));
-
-  console.log('here4');
-  app.listen(3000, () => {
-    console.log("App listening on http://localhost:3000");
-  });
-} catch (e) {
-  console.log('ERROR', JSON.stringify(e));
-}
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
