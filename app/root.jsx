67c56e3e7011e7bb187b0282b5efa699bff67e20
Nicholas Jordan <nijordan99@gmail.com>
2024-10-09 01:24:07 -0700

    Added favicon to build

--- app/root.jsx
+++ app/root.jsx
@@ -1,35 +1,51 @@
 import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-  } from "@remix-run/react";
-  
-  import './shared.css';
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+} from "@remix-run/react";
 
-  export const links = () => [
-    {
-      rel: 'icon',
-      href: '/assets/favicon.ico',
-      type: 'image/ico',
-    }
-  ];
+import favicon from '../assets/favicon.ico';
   
-  export default function App() {
-    return (
-      <html>
-        <head>
-          <link
-            rel="icon"
-            href="data:image/x-icon;base64,AA"
-          />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <Outlet />
-          <Scripts />
-        </body>
-      </html>
-    );
+import './shared.css';
+
+export const meta = () => [
+  {
+    title: 'Nicholas Jordan | Portfolio'
+  },
+  {
+    property: 'og:title',
+    content: 'Nicholas Jordan | Portfolio',
+  },
+  {
+    name: 'description',
+    content: 'Hey, my name is Nick and I enjoy solving problems.',
+  },
+];
+
+export const links = () => [
+  {
+    rel: 'icon',
+    href: favicon,
+    type: 'image/ico',
   }
+];
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link
+          rel="icon"
+          href="data:image/x-icon;base64,AA"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
