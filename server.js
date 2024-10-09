dc61a980891c84decd8ca45d69086f056ba99c36
Nicholas Jordan <nijordan99@gmail.com>
2024-10-08 22:50:20 -0700

    now dynamically importing index in server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import { createRequestHandler } from '@remix-run/express';
 import express from 'express';
-import * as build from "./index.js";
 
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  // const build = await import('index.js');
-  
+  const build = await import('./index.js');
+  app.all('*', createRequestHandler({ build }));
 } else {
   // const viteDevServer = await import('vite').then((vite) =>
   //   vite.createServer({
@@ -19,8 +18,6 @@ if (process.env.NODE_ENV === 'production') {
   // const build = () => viteDevServer.ssrLoadModule('virtual:remix/server-build');
   // app.all('*', createRequestHandler({ build }));
 }
-
-app.all('*', createRequestHandler({ build }));
 
 app.listen(3000, () => {
   console.log('App listening on http://localhost:3000');
